a21cb68af67092603397d96d2e83521aa5c0661e
Rangga857 <azharrangga28@gmail.com>
2025-05-08 00:15:55 +0700

    tambah event TodoEventComplete  untuk menandai todo selesai

--- lib/bloc/todo_bloc.dart
+++ lib/bloc/todo_bloc.dart
@@ -27,6 +27,25 @@ class TodoBloc extends Bloc<TodoEvent, TodoState> {
         emit(TodoLoaded(todos: currentState.todos, selectedDate: event.date));
       }
     });
-
+    on<TodoEventComplete>((event, emit) {
+      final currentState = state;
+      if (currentState is TodoLoaded) {
+        final List<Todo> updateTodos = List.from(currentState.todos);
+        if (event.index >= 0 && event.index < updateTodos.length) {
+          updateTodos[event.index] = Todo(
+            title: updateTodos[event.index].title,
+            //isCompleted: updateTodos[event.index].isCompleted == true,
+            isCompleted: !updateTodos[event.index].isCompleted,
+            date: updateTodos[event.index].date,
+          );
+          emit(
+          TodoLoaded(
+            todos: updateTodos,
+            selectedDate: currentState.selectedDate,
+          ),
+        );
+      } 
+      }
+    });
   }
 }
