16cb0b2a73a4910b6fffaf05c215c426f6424543
Rangga857 <azharrangga28@gmail.com>
2025-05-08 00:39:10 +0700

    Tambah Blocbuilder untuk menampilkan perubahan state

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -1,4 +1,6 @@
+import 'package:counterpage/bloc/todo_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TodoPage extends StatelessWidget {
   const TodoPage({super.key});
@@ -13,7 +15,25 @@ class TodoPage extends StatelessWidget {
           padding: EdgeInsets.all(16),
           child: Column(
             children: [
-              Text('Todo List')
+              Text('Todo List'),
+              Row(
+                children: [
+                  Text('Seleceted Data'),
+                  BlocBuilder<TodoBloc, TodoState>(
+                    builder: (context, state) {
+                      if(state is TodoLoaded){
+                        if(state.selectedDate != null){
+                          return Text(
+                            '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
+                          );
+                        }
+                      }
+                      return Text('No date selected');
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(width: 16.0,)
             ],
           ),)
       ),
