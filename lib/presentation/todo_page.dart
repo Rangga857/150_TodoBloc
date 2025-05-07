eb29039657d1a791945ae5b4a26a90e9c7f1c804
Rangga857 <azharrangga28@gmail.com>
2025-05-08 00:46:00 +0700

    Add Elevated date untuk menampilkan DatePicker

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -33,7 +33,24 @@ class TodoPage extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(width: 16.0,)
+              SizedBox(width: 16.0,),
+              ElevatedButton(
+                onPressed: (){
+                  showDatePicker(
+                    context: context, 
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000), 
+                    lastDate: DateTime(2100),
+                    ).then((selectedDate) {
+                      if (selectedDate != null){
+                        context.read<TodoBloc>().add(
+                          TodoSelectDate(date: selectedDate),
+                        );
+                      }
+                    }
+                  );
+                }, 
+                child: Text('Select Date'))
             ],
           ),)
       ),
