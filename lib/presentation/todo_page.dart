db4c754d0e9948a1a0b89a60cb7e5d0e9ea56f3c
Rangga857 <azharrangga28@gmail.com>
2025-05-08 00:27:37 +0700

    Add text todolist

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -9,7 +9,13 @@ class TodoPage extends StatelessWidget {
     final _controller = TextEditingController();
     return Scaffold(
       body: SafeArea(
-        child: 
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Text('Todo List')
+            ],
+          ),)
       ),
     );
   }
