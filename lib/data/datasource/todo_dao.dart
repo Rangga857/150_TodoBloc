448774fd637ba61cdc3cd30769380d2ce103826c
Rangga857 <azharrangga28@gmail.com>
2025-05-23 15:57:14 +0700

    create dao

---
+++ lib/data/datasource/todo_dao.dart
@@ -0,0 +1,41 @@
+import 'package:counterpage/data/datasource/database_helper.dart';
+import 'package:counterpage/data/model/todo.dart';
+
+class TodoDao {
+  final dbProvider = DatabaseHelper();
+
+  Future<int> insert(Todo todo) async {
+    final db = await dbProvider.database;
+    return await db.insert('todos', todo.toMap());
+  }
+
+  Future<List<Todo>> getAllTodos() async {
+    final db = await dbProvider.database;
+    final result = await db.query('todo', orderBy: 'date DESC');
+    final rows = await db.query('todo');
+
+    // log add data
+    rows.forEach((row) => print(row));
+    return result.map((e) => Todo.fromMap(e)).toList();
+  }
+
+  Future<int> update(Todo todo) async {
+    final db = await dbProvider.database;
+    return await db.update(
+      'todo',
+      todo.toMap(),
+      where: 'id = ?',
+      whereArgs: [todo.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await dbProvider.database;
+    return await db.delete(
+      'todo',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+}
